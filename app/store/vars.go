ab840918279af1100eeb67402ea562e5289ecfe1
Yelshat Duskaliyev <ura2178@gmail.com>
2021-12-13 00:02:09 +0300

    removed redundant evaluated field, made Vars an alias for a map

--- app/store/vars.go
+++ app/store/vars.go
@@ -11,61 +11,60 @@ import (
 
 // VarsFromMap makes a new instance of Vars, not evaluated yet but filled
 // with the provided map.
-func VarsFromMap(m map[string]string) Vars { return Vars{vals: m} }
+func VarsFromMap(m map[string]string) Vars { return m }
 
 // Vars is an alias for a map with variable values.
-type Vars struct {
-	vals      map[string]string
-	evaluated bool
-}
+type Vars map[string]string
 
 // UnmarshalYAML implements unmarshaler in order to parse the map of values.
 func (v *Vars) UnmarshalYAML(value *yaml.Node) error {
-	return value.Decode(&v.vals)
+	m := map[string]string{}
+	if err := value.Decode(&m); err != nil {
+		return err
+	}
+	*v = m
+	return nil
 }
 
 // Has returns true if variable with specified key is present.
 func (v *Vars) Has(key string) bool {
-	if v.vals == nil {
-		v.vals = map[string]string{}
+	if v == nil {
+		*v = map[string]string{}
 	}
-	_, ok := v.vals[key]
+	_, ok := (*v)[key]
 	return ok
 }
 
 // Get returns the value of the variable.
 func (v Vars) Get(name string) string {
-	if v.vals == nil {
-		v.vals = map[string]string{}
+	if v == nil {
+		v = map[string]string{}
 	}
-	return v.vals[name]
+	return v[name]
 }
 
 // Set sets the value of the variable.
 func (v *Vars) Set(name, val string) {
-	if v.vals == nil {
-		v.vals = map[string]string{}
+	if v == nil {
+		*v = map[string]string{}
 	}
-	v.vals[name] = val
+	(*v)[name] = val
 }
 
 // List returns a list of strings from var's
 // value parsed in form of "string1,string2,string3"
 func (v Vars) List(s string) []string { return strings.Split(v.Get(s), ",") }
 
-// Evaluated returns true if vars contains the set of already evaluated values.
-func (v Vars) Evaluated() bool { return v.evaluated }
-
 type evTmpl struct{ Update Update }
 
 // Evaluate evaluates the final values of each variable.
 func (v Vars) Evaluate(upd Update) (Vars, error) {
-	if len(v.vals) == 0 {
-		return Vars{evaluated: true}, nil
+	if len(v) == 0 {
+		return nil, nil
 	}
 
-	res := Vars{vals: map[string]string{}, evaluated: true}
-	for key, vv := range v.vals {
+	res := Vars(map[string]string{})
+	for key, vv := range v {
 		tmpl, err := template.New("").Funcs(funcs).Parse(vv)
 		if err != nil {
 			return Vars{}, fmt.Errorf("parse %q variable: %w", key, err)
@@ -76,7 +75,7 @@ func (v Vars) Evaluate(upd Update) (Vars, error) {
 			return Vars{}, fmt.Errorf("evaluate the value of the %q variable: %w", key, err)
 		}
 
-		res.vals[key] = buf.String()
+		res[key] = buf.String()
 	}
 
 	return res, nil
@@ -87,12 +86,12 @@ func (v Vars) Evaluate(upd Update) (Vars, error) {
 // to be equal, so the Evaluated state, in case if important, must be checked
 // separately.
 func (v Vars) Equal(oth Vars) bool {
-	if len(v.vals) != len(oth.vals) {
+	if len(v) != len(oth) {
 		return false
 	}
 
-	for key, val := range v.vals {
-		othVal, present := oth.vals[key]
+	for key, val := range v {
+		othVal, present := oth[key]
 		if !present || val != othVal {
 			return false
 		}
--- app/store/vars_test.go
+++ app/store/vars_test.go
@@ -12,18 +12,18 @@ func TestVars_UnmarshalYAML(t *testing.T) {
 	v := Vars{}
 	err := yaml.Unmarshal([]byte(y), &v)
 	assert.NoError(t, err)
-	assert.Equal(t, Vars{vals: map[string]string{"var1": "val1", "var2": "val2"}}, v)
+	assert.Equal(t, Vars(map[string]string{"var1": "val1", "var2": "val2"}), v)
 }
 
 func TestVars_Has(t *testing.T) {
-	v := &Vars{vals: map[string]string{"var1": "val1"}}
+	v := Vars(map[string]string{"var1": "val1"})
 	assert.True(t, v.Has("var1"))
 	assert.False(t, v.Has("var2"))
 	assert.NotPanics(t, func() { assert.False(t, (&Vars{}).Has("blah")) })
 }
 
 func TestVars_Get(t *testing.T) {
-	v := &Vars{vals: map[string]string{"var1": "val1"}}
+	v := Vars(map[string]string{"var1": "val1"})
 	assert.Equal(t, "val1", v.Get("var1"))
 	assert.Empty(t, v.Get("var2"))
 	assert.NotPanics(t, func() { assert.Empty(t, (&Vars{}).Get("var1")) })
@@ -32,42 +32,32 @@ func TestVars_Get(t *testing.T) {
 func TestVars_Set(t *testing.T) {
 	v := Vars{}
 	v.Set("var1", "val1")
-	assert.Equal(t, Vars{vals: map[string]string{"var1": "val1"}}, v)
+	assert.Equal(t, Vars(map[string]string{"var1": "val1"}), v)
 	assert.NotPanics(t, func() { (&Vars{}).Set("var1", "val1") })
 }
 
 func TestVars_List(t *testing.T) {
-	v := Vars{vals: map[string]string{"list": "a,b,c,d"}}
+	v := Vars(map[string]string{"list": "a,b,c,d"})
 	assert.Equal(t, []string{"a", "b", "c", "d"}, v.List("list"))
 	assert.NotPanics(t, func() { (&Vars{}).List("var1") })
 }
 
-func TestVars_Evaluated(t *testing.T) {
-	v := Vars{evaluated: true}
-	assert.True(t, v.Evaluated())
-	assert.False(t, Vars{}.Evaluated())
-}
-
 func TestVars_Equal(t *testing.T) {
-	assert.True(t, Vars{vals: map[string]string{"var1": "val1", "var2": "val2"}}.
-		Equal(Vars{vals: map[string]string{"var2": "val2", "var1": "val1"}}))
+	assert.True(t, Vars(map[string]string{"var1": "val1", "var2": "val2"}).
+		Equal(map[string]string{"var2": "val2", "var1": "val1"}))
 	assert.True(t, Vars{}.Equal(Vars{}))
 
-	// evaluated state is not considered in Equal
-	assert.True(t, Vars{evaluated: true}.Equal(Vars{evaluated: true}))
-	assert.True(t, Vars{evaluated: true}.Equal(Vars{evaluated: false}))
-
-	assert.False(t, Vars{vals: map[string]string{"var1": "val1"}}.
-		Equal(Vars{vals: map[string]string{"var2": "val2", "var1": "val1"}}))
+	assert.False(t, Vars(map[string]string{"var1": "val1"}).
+		Equal(map[string]string{"var2": "val2", "var1": "val1"}))
 }
 
 func TestVars_Evaluate(t *testing.T) {
-	v := Vars{vals: map[string]string{
+	v := Vars(map[string]string{
 		"var1": `{{ env "TESTVAR" }}`,
 		"var2": `{{ seq (keys .Update.Fields) }}`,
 		"var3": `{{ seq (values .Update.Fields) }}`,
 		"var4": `static text`,
-	}}
+	})
 	err := os.Setenv("TESTVAR", "blah")
 	vs, err := v.Evaluate(Update{Content: Content{Fields: TicketFields{
 		"f1": "f1v",
@@ -76,9 +66,8 @@ func TestVars_Evaluate(t *testing.T) {
 	assert.NoError(t, err)
 
 	// checking field-by-field, as mustn't rely on order of walking over map
-	assert.True(t, vs.evaluated)
-	assert.Equal(t, "blah", vs.vals["var1"])
-	assert.Contains(t, []string{"f1,f2", "f2,f1"}, vs.vals["var2"])
-	assert.Contains(t, []string{"f1v,f2v", "f2v,f1v"}, vs.vals["var3"])
-	assert.Equal(t, "static text", vs.vals["var4"])
+	assert.Equal(t, "blah", vs["var1"])
+	assert.Contains(t, []string{"f1,f2", "f2,f1"}, vs["var2"])
+	assert.Contains(t, []string{"f1v,f2v", "f2v,f1v"}, vs["var3"])
+	assert.Equal(t, "static text", vs["var4"])
 }
